6a8bf62eb0d868e306a71216777934eee1b9c236
Jennifer Wagner <jennifer.wagner@tum.de>
2025-11-15 21:50:02 +0100

    add guard aiagent

---
+++ trash-horror/Assets/Scripts/AIAgents/Guard.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Guard : Hostile
+{
+    private UnityEngine.AI.NavMeshAgent agent;
+    protected override void Start()
+    {
+        base.Start();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    protected override void Chasing()
+    {
+        base.Chasing();
+        agent.destination = target.position;
+    }
+}
+
