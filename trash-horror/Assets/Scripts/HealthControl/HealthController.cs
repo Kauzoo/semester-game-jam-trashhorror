2c3204eaf268704d4102b114175c1456c06558c7
duckin <duckin@protonmail.com>
2025-11-15 16:32:08 +0100

    Add Health SO & Health Bar

---
+++ trash-horror/Assets/Scripts/HealthControl/HealthController.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public FloatVariable healthData;
+    public GameEvent onHealthChanged;
+    
+    public void DecreaseHealth(float amount)
+    {
+        healthData.value -= amount;
+        onHealthChanged.Raise();
+    }
+
+    public void IncreaseHealth(float amount)
+    {
+        healthData.value += amount;
+        onHealthChanged.Raise();
+    }
+}
