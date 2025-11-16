59f35199152298a5976800f90ca14d12989ad67b
Nyr <33641148+Kauzoo@users.noreply.github.com>
2025-11-16 11:45:16 +0100

    SOUND

--- trash-horror/Assets/Scripts/SpikeController.cs
+++ trash-horror/Assets/Scripts/SpikeController.cs
@@ -8,12 +8,5 @@ public class SpikeController : Instant
     protected override void Trigger(PlayerBehaviour player)
     {
         HealthController.Instance.DecreaseHealth(1);
-        List<GameObject> res = new();
-        GameObject.FindGameObjectsWithTag("AudioHandler", res);
-        if (res[0] != null)
-        {
-            var audio_handler = res[0].GetComponent<AudioHandler>();
-            audio_handler.PlayTrapDamage();
-        }
     }
 }
